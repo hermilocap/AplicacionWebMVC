7cc6e6dee28ec400e0335038f1ba1eabecd640ea
hermilocap <hermilocap@hotmail.com>
2018-07-31 22:43:41 -0500

    Apliacion web MVC

---
+++ AplicacionWebMVC/Controllers/PersonaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionWebMVC.Models;
+
+namespace AplicacionWebMVC.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            List<Persona> lista = new List<Persona>();
+
+            Persona persona = new Persona();
+            persona.codigo = 1;
+            persona.nombre = "Hermilo";
+            persona.apellido = "Santiago";
+            lista.Add(persona);
+
+            Persona persona2 = new Persona();
+            persona2.codigo = 2;
+            persona2.nombre = "Andrea";
+            persona2.apellido = "Bracamontes";
+            lista.Add(persona2);
+
+            Persona persona3 = new Persona();
+            persona3.codigo = 3;
+            persona3.nombre = "Pedro";
+            persona3.apellido = "Lopez";
+            lista.Add(persona3);
+
+            return View(lista);
+        }
+    }
+}
---
+++ AplicacionWebMVC/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionWebMVC.Models
+{
+    public class Persona
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
